f56891f3b0e148c83c20503661d4ff4eb66634b4
supakornza <k.supakorn1997@gmail.com>
2026-05-16 03:03:09 +0700

    Update dashboard performance and production settings

--- config/settings/development.py
+++ config/settings/development.py
@@ -4,8 +4,12 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'construction_erp',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
