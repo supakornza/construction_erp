3f6e1f6ca493786b61737977d55e97f1f8841154
Supakorn <k.supakorn1997@gmail.com>
2026-05-16 22:30:53 +0700

    modified:   config/settings/development.py

--- config/settings/development.py
+++ config/settings/development.py
@@ -3,14 +3,7 @@ from .base import *
 DEBUG = True
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'construction_erp',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': env.db('DATABASE_URL', default='sqlite:///db.sqlite3')
 }
 
 INTERNAL_IPS = ['127.0.0.1']
